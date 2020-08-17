67d1ba8bbe8a42f1cd66e607ada224a45e6ad2cb
KimMinji <happy0652@naver.com>
2020-08-17 20:58:25 +0900

    // 채영_1.manifests // 채영_2.MainActivity,friendList.kt // 채영_3.activity_main.xml,activity_friend_list.xml, call_all.xml, call_fail.xml, call_ok.xml, call_one.xml, item_friends.xml

--- app/src/main/java/com/example/cho6/guru2_android12/MainActivity.kt
+++ app/src/main/java/com/example/cho6/guru2_android12/MainActivity.kt
@@ -1,11 +1,485 @@
 package com.example.cho6.guru2_android12
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.util.*
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback,
+    OnRequestPermissionsResultCallback {
+    private var mMap: GoogleMap? = null
+    private var currentMarker: Marker? = null
+    var needRequest = false
+
+    // 앱을 실행하기 위해 필요한 퍼미션을 정의합니다.
+    var REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) // 외부 저장소
+    var mCurrentLocatiion: Location? = null
+    var currentPosition: LatLng? = null
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    private var locationRequest: LocationRequest? = null
+    private var location: Location? = null
+    private var mLayout // Snackbar 사용하기 위해서는 View가 필요합니다.
+            : View? = null
+
+    // (참고로 Toast에서는 Context가 필요했습니다.)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        )
         setContentView(R.layout.activity_main)
+        mLayout = findViewById(R.id.layout_main)
+        locationRequest = LocationRequest()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(UPDATE_INTERVAL_MS.toLong())
+            .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS.toLong())
+        val locationmultical=locationRequest
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(locationmultical!!)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+
+        // 위드미 버튼 클릭 리스너
+        withmeButton.setOnClickListener {
+            val nextIntent= Intent(this, friendList::class.java)
+            startActivity(nextIntent)
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        Log.d(TAG, "onMapReady :")
+        mMap = googleMap
+
+        //런타임 퍼미션 요청 대화상자나 GPS 활성 요청 대화상자 보이기전에
+        //지도의 초기위치를 서울로 이동
+        setDefaultLocation()
+
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+            hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+            startLocationUpdates() // 3. 위치 업데이트 시작
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    REQUIRED_PERMISSIONS[0]
+                )
+            ) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Snackbar.make(
+                    mLayout!!, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.",
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(
+                    "확인",
+                    View.OnClickListener { // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity, REQUIRED_PERMISSIONS,
+                            PERMISSIONS_REQUEST_CODE
+                        )
+                    }).show()
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(
+                    this, REQUIRED_PERMISSIONS,
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+        }
+        mMap!!.uiSettings.isMyLocationButtonEnabled = true
+        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(15f))
+        mMap!!.setOnMapClickListener { Log.d(TAG, "onMapClick :") }
+    }
+
+    var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            val locationList =
+                locationResult.locations
+            if (locationList.size > 0) {
+                location = locationList[locationList.size - 1]
+                //location = locationList.get(0);
+                currentPosition = LatLng(location!!.latitude, location!!.longitude)
+                val markerTitle = getCurrentAddress(currentPosition!!)
+                val markerSnippet =
+                    "위도:" + location!!.latitude.toString() + " 경도:" + location!!.longitude
+                        .toString()
+                Log.d(
+                    TAG,
+                    "onLocationResult : $markerSnippet"
+                )
+
+
+                //현재 위치에 마커 생성하고 이동
+                setCurrentLocation(location, markerTitle, markerSnippet)
+                mCurrentLocatiion = location
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+        if (!checkLocationServicesStatus()) {
+            Log.d(
+                TAG,
+                "startLocationUpdates : call showDialogForLocationServiceSetting"
+            )
+            showDialogForLocationServiceSetting()
+        } else {
+            val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED ||
+                hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.d(TAG, "startLocationUpdates : 퍼미션 안가지고 있음")
+                return
+            }
+            Log.d(
+                TAG,
+                "startLocationUpdates : call mFusedLocationClient.requestLocationUpdates"
+            )
+            mFusedLocationClient!!.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.myLooper()
+            )
+            if (checkPermission()) mMap!!.isMyLocationEnabled = true
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+        if (checkPermission()) {
+            Log.d(
+                TAG,
+                "onStart : call mFusedLocationClient.requestLocationUpdates"
+            )
+            mFusedLocationClient!!.requestLocationUpdates(locationRequest, locationCallback, null)
+            if (mMap != null) mMap!!.isMyLocationEnabled = true
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mFusedLocationClient != null) {
+            Log.d(TAG, "onStop : call stopLocationUpdates")
+            mFusedLocationClient!!.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    fun getCurrentAddress(latlng: LatLng): String {
+
+        //지오코더... GPS를 주소로 변환
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses: List<Address>
+        addresses = try {
+            geocoder.getFromLocation(
+                latlng.latitude,
+                latlng.longitude,
+                1
+            )
+        } catch (ioException: IOException) {
+            //네트워크 문제
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show()
+            return "지오코더 서비스 사용불가"
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show()
+            return "잘못된 GPS 좌표"
+        }
+        return if (addresses == null || addresses.size == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show()
+            "주소 미발견"
+        } else {
+            val address = addresses[0]
+            address.getAddressLine(0).toString()
+        }
+    }
+
+    fun checkLocationServicesStatus(): Boolean {
+        val locationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+    }
+
+    fun setCurrentLocation(
+        location: Location?,
+        markerTitle: String?,
+        markerSnippet: String?
+    ) {
+        if (currentMarker != null) currentMarker!!.remove()
+        val currentLatLng = LatLng(location!!.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(currentLatLng)
+        markerOptions.title(markerTitle)
+        markerOptions.snippet(markerSnippet)
+        markerOptions.draggable(true)
+        currentMarker = mMap!!.addMarker(markerOptions)
+        val cameraUpdate = CameraUpdateFactory.newLatLng(currentLatLng)
+        mMap!!.moveCamera(cameraUpdate)
+    }
+
+    fun setDefaultLocation() {
+
+
+        //디폴트 위치, Seoul
+        val DEFAULT_LOCATION = LatLng(37.56, 126.97)
+        val markerTitle = "위치정보 가져올 수 없음"
+        val markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요"
+        if (currentMarker != null) currentMarker!!.remove()
+        val markerOptions = MarkerOptions()
+        markerOptions.position(DEFAULT_LOCATION)
+        markerOptions.title(markerTitle)
+        markerOptions.snippet(markerSnippet)
+        markerOptions.draggable(true)
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+        currentMarker = mMap!!.addMarker(markerOptions)
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 15f)
+        mMap!!.moveCamera(cameraUpdate)
+    }
+
+    //여기부터는 런타임 퍼미션 처리을 위한 메소드들
+    private fun checkPermission(): Boolean {
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        return if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+            hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED
+        ) {
+            true
+        } else false
+    }
+
+    /*
+     * ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드입니다.
+     */
+    override fun onRequestPermissionsResult(
+        permsRequestCode: Int,
+        permissions: Array<String>,
+        grandResults: IntArray
+    ) {
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.size == REQUIRED_PERMISSIONS.size) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+            var check_result = true
+
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            for (result in grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false
+                    break
+                }
+            }
+            if (check_result) {
+
+                // 퍼미션을 허용했다면 위치 업데이트를 시작합니다.
+                startLocationUpdates()
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[0]
+                    )
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        REQUIRED_PERMISSIONS[1]
+                    )
+                ) {
+
+
+                    // 사용자가 거부만 선택한 경우에는 앱을 다시 실행하여 허용을 선택하면 앱을 사용할 수 있습니다.
+                    Snackbar.make(
+                        mLayout!!, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요. ",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction("확인", View.OnClickListener { finish() }).show()
+                } else {
+
+
+                    // "다시 묻지 않음"을 사용자가 체크하고 거부를 선택한 경우에는 설정(앱 정보)에서 퍼미션을 허용해야 앱을 사용할 수 있습니다.
+                    Snackbar.make(
+                        mLayout!!, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction("확인", View.OnClickListener { finish() }).show()
+                }
+            }
+        }
+    }
+
+    //여기부터는 GPS 활성화를 위한 메소드들
+    private fun showDialogForLocationServiceSetting() {
+        val builder =
+            AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage(
+            """
+                앱을 사용하기 위해서는 위치 서비스가 필요합니다.
+                위치 설정을 수정하실래요?
+                """.trimIndent()
+        )
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정") { dialog, id ->
+            val callGPSSettingIntent =
+                Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivityForResult(
+                callGPSSettingIntent,
+                GPS_ENABLE_REQUEST_CODE
+            )
+        }
+        builder.setNegativeButton(
+            "취소"
+        ) { dialog, id -> dialog.cancel() }
+        builder.create().show()
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GPS_ENABLE_REQUEST_CODE ->
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+                        Log.d(
+                            TAG,
+                            "onActivityResult : GPS 활성화 되있음"
+                        )
+                        needRequest = true
+                        return
+                    }
+                }
+        }
+    }
+
+    companion object {
+        private const val TAG = "googlemap_example"
+        private const val GPS_ENABLE_REQUEST_CODE = 2001
+        private const val UPDATE_INTERVAL_MS = 1000 // 1초
+        private const val FASTEST_UPDATE_INTERVAL_MS = 500 // 0.5초
+
+        // onRequestPermissionsResult에서 수신된 결과에서 ActivityCompat.requestPermissions를 사용한 퍼미션 요청을 구별하기 위해 사용됩니다.
+        private const val PERMISSIONS_REQUEST_CODE = 100
     }
 }
+////import android.R
+//import android.content.Intent
+//import com.guru.withmefriend.R
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.lifecycle.Transformations.map
+////import androidx.lifecycle.Transformations.map
+//import com.google.android.gms.maps.CameraUpdateFactory
+//import com.google.android.gms.maps.GoogleMap
+//import com.google.android.gms.maps.OnMapReadyCallback
+//import com.google.android.gms.maps.SupportMapFragment
+//import com.google.android.gms.maps.model.LatLng
+//import com.google.android.gms.maps.model.MarkerOptions
+//import kotlinx.android.synthetic.main.activity_main.*
+//
+//
+//class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+//    private var mMap: GoogleMap? = null
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.map) as SupportMapFragment?
+//        mapFragment!!.getMapAsync(this)
+//
+//        withmeButton.setOnClickListener {
+//            val nextIntent= Intent(this, friendList::class.java)
+//            startActivity(nextIntent)
+//        }
+//    }
+//
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        mMap = googleMap
+//        val SEOUL = LatLng(37.56, 126.97)
+//        val markerOptions = MarkerOptions()
+//        markerOptions.position(SEOUL)
+//        markerOptions.title("서울")
+//        markerOptions.snippet("한국의 수도")
+//        mMap!!.addMarker(markerOptions)
+//        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(SEOUL))
+//        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(10f))
+//    }
+//}
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        withmeButton.setOnClickListener {
+//            val nextIntent=Intent(this, friendList::class.java)
+//            startActivity(nextIntent)
+//        }
+//    }
+//
+//}
---
+++ app/src/main/java/com/example/cho6/guru2_android12/friendList.kt
@@ -0,0 +1,132 @@
+package com.example.cho6.guru2_android12
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cho6.guru2_android12.databinding.ActivityFriendListBinding
+import com.example.cho6.guru2_android12.databinding.ItemFriendsBinding
+
+class friendList : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFriendListBinding
+
+    private val data= arrayListOf<Friends>()
+
+    var myDialog: Dialog?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityFriendListBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        data.add(Friends("김채영", "247"))
+        data.add(Friends("정지연", "256"))
+
+        binding.recyclerView.layoutManager=LinearLayoutManager(this)
+        binding.recyclerView.adapter=FriendListAdapter(data)
+
+        myDialog=Dialog(this)
+    }
+
+    fun ShowCallOnePop(v:View){
+        val textView:TextView
+        val butClose:Button
+        val butNo:Button
+        val butYes:Button
+        myDialog?.setContentView(R.layout.call_one)
+        textView= myDialog?.findViewById(R.id.nickname_call_one) as TextView
+        butClose= myDialog?.findViewById(R.id.close_call_one) as Button
+        butYes=myDialog?.findViewById(R.id.yes_call_one) as Button
+        butNo=myDialog?.findViewById(R.id.no_call_one) as Button
+        butClose.setOnClickListener {
+            myDialog!!.dismiss()
+        }
+        butNo.setOnClickListener {
+            myDialog!!.dismiss()
+            val failedWindow:Button= myDialog?.findViewById(R.id.no_call_one)!!
+            ShowNoPop(failedWindow)
+        }
+        butYes.setOnClickListener {
+            myDialog!!.dismiss()
+            val okWindow:Button= myDialog?.findViewById(R.id.yes_call_one)!!
+            ShowYesPop(okWindow)
+        }
+        myDialog!!.show()
+    }
+
+    fun ShowCallAllPop(v:View){
+        val textView:TextView
+        val butClose:Button
+        val butNo:Button
+        val butYes:Button
+        myDialog?.setContentView(R.layout.call_all)
+        textView= myDialog?.findViewById(R.id.nickname_call_one) as TextView
+        butClose= myDialog?.findViewById(R.id.close_call_one) as Button
+        butYes=myDialog?.findViewById(R.id.yes_call_one) as Button
+        butNo=myDialog?.findViewById(R.id.no_call_one) as Button
+        butClose.setOnClickListener {
+            myDialog!!.dismiss()
+        }
+        butNo.setOnClickListener {
+            myDialog!!.dismiss()
+            val failedWindow:Button= myDialog?.findViewById(R.id.no_call_one)!!
+            ShowNoPop(failedWindow)
+        }
+        butYes.setOnClickListener {
+            myDialog!!.dismiss()
+            val okWindow:Button= myDialog?.findViewById(R.id.yes_call_one)!!
+            ShowYesPop(okWindow)
+        }
+        myDialog!!.show()
+    }
+
+    fun ShowYesPop(v:View){
+        val butClose:Button
+        myDialog?.setContentView(R.layout.call_ok)
+        butClose= myDialog?.findViewById(R.id.close_call_one) as Button
+        butClose.setOnClickListener {
+            myDialog!!.dismiss()
+        }
+        myDialog!!.show()
+    }
+
+    fun ShowNoPop(v:View){
+        val butClose:Button
+        myDialog?.setContentView(R.layout.call_fail)
+        butClose= myDialog?.findViewById(R.id.close_call_one) as Button
+        butClose.setOnClickListener {
+            myDialog!!.dismiss()
+        }
+        myDialog!!.show()
+    }
+}
+
+data class Friends(val nickname:String, var distance:String)
+
+class FriendListAdapter(private val myDataset: List<Friends>) :
+    RecyclerView.Adapter<FriendListAdapter.FriendListViewHolder>() {
+
+    class FriendListViewHolder(val binding: ItemFriendsBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): FriendListAdapter.FriendListViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_friends, parent, false)
+        return FriendListViewHolder(ItemFriendsBinding.bind(view))
+    }
+
+    override fun onBindViewHolder(holder: FriendListViewHolder, position: Int) {
+        holder.binding.nickname.text=myDataset[position].nickname
+        holder.binding.distance.text=myDataset[position].distance
+    }
+
+    override fun getItemCount() = myDataset.size
+}
