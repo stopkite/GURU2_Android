f2a9e918173aed645a7cf5f82596f6c8b568f54c
chaeyoung <iiblueblue@naver.com>
2020-08-20 05:11:59 +0900

    위드미 누르면 현재 위치 반환되는 getAdapterPosition 함수 사용

--- app/src/main/java/com/example/cho6/guru2_android12/friendList.kt
+++ app/src/main/java/com/example/cho6/guru2_android12/friendList.kt
@@ -10,13 +10,11 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.cho6.guru2_android12.databinding.ActivityFriendListBinding
 import com.example.cho6.guru2_android12.databinding.ItemFriendsBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -34,6 +32,9 @@ class friendList : AppCompatActivity() {
     private val friendListViewModel: FriendListViewModel by viewModels()
 
     var myDialog: Dialog? = null
+
+    //테스트
+    var selectData:String=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -84,6 +85,18 @@ class friendList : AppCompatActivity() {
             myDialog!!.dismiss()
             val okWindow: Button = myDialog?.findViewById(R.id.yes_call_one)!!
             ShowYesPop(okWindow)
+
+
+            //정보 전달 테스트
+            Log.d("gotest", "")
+
+            // 채팅방으로 정보 전달
+//            val nextIntent= Intent(this, ChatRoomActivity::class.java)
+//            nextIntent.apply {
+//                this.putExtra("useruid", friendListViewModel.)  //이걸 연구해야함
+//            }
+//            startActivity(nextIntent)
+
         }
         myDialog!!.show()
     }
@@ -153,6 +166,8 @@ class FriendListAdapter(private var myDataset: List<Friends>) :
     class FriendListViewHolder(val binding: ItemFriendsBinding) :
         RecyclerView.ViewHolder(binding.root)
 
+    var goChatData: String=""
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -166,6 +181,18 @@ class FriendListAdapter(private var myDataset: List<Friends>) :
         holder.binding.nickname.text = myDataset[position].nickname
         holder.binding.distance.text = myDataset[position].distance.toString()
         Picasso.get().load(myDataset[position].pic).into(holder.binding.frienditempic)
+
+        // 버튼을 누르면 현재 누른 아이템이 몇번째 아이템인지 반환
+        holder.binding.withmeone.setOnClickListener {
+            val pos: Int = holder.getAdapterPosition()
+            goChatData=myDataset[pos].userid
+        }
+
+
+    }
+
+    public fun getChatData():String{
+        return goChatData
     }
 
     override fun getItemCount() = myDataset.size
@@ -182,6 +209,10 @@ class FriendListViewModel : ViewModel() {
     val db = Firebase.firestore
 
     private val data = arrayListOf<Friends>()
+
+    fun getData():ArrayList<Friends>{
+        return data
+    }
 
 
     // 내 위치를 데이터베이스에 저장하기 위한 함수=>성공
